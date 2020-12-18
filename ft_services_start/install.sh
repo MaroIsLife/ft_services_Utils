933263f76a71ddafac6219eff789b7e8a5cd44bf
MaroIsLife <ougnou10@gmail.com>
2020-12-19 00:59:49 +0100

    Update install.sh

--- ft_services_start/install.sh
+++ ft_services_start/install.sh
@@ -5,9 +5,9 @@ brew install minikube
 brew install docker
 ln -s /goinfre/$USERNAME ~/goinfre
 mv ~/.brew /goinfre/$USERNAME/
-ln -s ~/goinfre/$USERNAME/.brew ~/.brew
-mv ~/.docker /goinfre/$USERNAME/
-ln -s ~/goinfre/$USERNAME/.docker ~/.docker
+#ln -s ~/goinfre/$USERNAME/.brew ~/.brew
+#mv ~/.docker /goinfre/$USERNAME/
+#ln -s ~/goinfre/$USERNAME/.docker ~/.docker
 brew install docker-machine
 docker-machine create --driver virtualbox default
 sh envdocker.sh
