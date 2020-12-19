c8a2c5d428b504714a4e42e499d8b4345390618a
MaroIsLife <ougnou10@gmail.com>
2020-12-19 06:00:35 +0100

    Update install.sh

--- ft_services_start/install.sh
+++ ft_services_start/install.sh
@@ -3,11 +3,11 @@ curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
 rm -rf ~/goinfre && ln -s /goinfre/$USERNAME ~/goinfre
 brew install minikube
 brew install docker
+brew install docker-machine
 ln -s /goinfre/$USERNAME ~/goinfre
 mv ~/.brew /goinfre/$USERNAME/
 ln -s ~/goinfre/$USERNAME/.brew ~/.brew
 #mv ~/.docker /goinfre/$USERNAME/
 #ln -s ~/goinfre/$USERNAME/.docker ~/.docker
-brew install docker-machine
 docker-machine create --driver virtualbox default
 sh envdocker.sh
