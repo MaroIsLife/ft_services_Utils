eae4a8ae0ed49081849fa2b55767544a2f1a4e85
MaroIsLife <ougnou10@gmail.com>
2021-01-16 12:03:39 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl delete -f srcs/yamls/$1.yaml
-eval $(minikube -p minikube docker-env)
+#eval $(minikube -p minikube docker-env)
 docker build -f srcs/$1/Dockerfile -t image_$1 .
 kubectl create -f srcs/yamls/$1.yaml
