ee706046f1c0fe69efa82349a9ad69e1d713f1a2
MaroIsLife <ougnou10@gmail.com>
2021-01-16 16:20:47 +0100

    Update nodestart.sh

--- ft_services_start/nodestart.sh
+++ ft_services_start/nodestart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MINIKUBE_HOME=/goinfre/$USERNAME
-minikube start --extra-config=apiserver.service-node-port-range=1-30000
+minikube start --driver virtualbox 
 eval $(minikube -p minikube docker-env)
